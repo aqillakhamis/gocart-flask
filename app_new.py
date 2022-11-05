15843b754ad8ad0efa4f23b61df12821c9866775
aqillakhamis <aqillakhamis@gmail.com>
2022-11-05 09:30:37 +0800

    push app_new and html_new.py

---
+++ app_new.py
@@ -0,0 +1,223 @@
+import argparse
+import base64
+import json
+import os
+import platform
+import sys
+from pathlib import Path
+
+import cv2
+import torch
+from flask import Flask, render_template
+from flask_sock import Sock
+
+from models.common import DetectMultiBackend
+from utils.dataloaders import (IMG_FORMATS, VID_FORMATS, LoadImages,
+                               LoadScreenshots, LoadStreams)
+from utils.general import (LOGGER, Profile, check_file, check_img_size,
+                           check_imshow, cv2, increment_path,
+                           non_max_suppression, scale_boxes, xyxy2xywh)
+from utils.plots import Annotator, colors, save_one_box
+from utils.torch_utils import select_device
+
+app = Flask(__name__)
+sock = Sock(app)
+
+
+@app.route('/')
+def index():
+    """Video streaming home page."""
+    return render_template('index_new.html')
+
+
+@sock.route('/data')
+def echo(ws):
+    FILE = Path(__file__).resolve()
+    ROOT = FILE.parents[0]  # YOLOv5 root directory
+    if str(ROOT) not in sys.path:
+        sys.path.append(str(ROOT))  # add ROOT to PATH
+    ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--weights', nargs='+', type=str, default=ROOT / 'best_aug.pt', help='model path or triton URL')
+    # parser.add_argument('--weights', nargs='+', type=str,
+                        # default=ROOT / 'best_aug.pt', help='model path or triton URL')
+    # parser.add_argument('--source', type=str, default='0',
+                        # help='file/dir/URL/glob/screen/0(webcam)')
+    parser.add_argument('--source', type=str, default='http://192.168.214.240:8080/video', help='file/dir/URL/glob/screen/0(webcam)')
+    parser.add_argument('--data', type=str, default=ROOT /
+                        'data.yaml', help='(optional) dataset.yaml path')
+    parser.add_argument('--imgsz', '--img', '--img-size', nargs='+',
+                        type=int, default=[416], help='inference size h,w')
+    parser.add_argument('--conf-thres', type=float,
+                        default=0.50, help='confidence threshold')
+    parser.add_argument('--iou-thres', type=float,
+                        default=0.45, help='NMS IoU threshold')
+    parser.add_argument('--max-det', type=int, default=1000,
+                        help='maximum detections per image')
+    parser.add_argument('--device', default='',
+                        help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+    parser.add_argument('--view-img', action='store_true', help='show results')
+    parser.add_argument('--save-txt', action='store_true',
+                        help='save results to *.txt')
+    parser.add_argument('--save-conf', action='store_true',
+                        help='save confidences in --save-txt labels')
+    parser.add_argument('--save-crop', action='store_true',
+                        help='save cropped prediction boxes')
+    parser.add_argument('--nosave', action='store_true',
+                        help='do not save images/videos')
+    parser.add_argument('--classes', nargs='+', type=int,
+                        help='filter by class: --classes 0, or --classes 0 2 3')
+    parser.add_argument('--agnostic-nms', action='store_true',
+                        help='class-agnostic NMS')
+    parser.add_argument('--augment', action='store_true',
+                        help='augmented inference')
+    parser.add_argument('--visualize', action='store_true',
+                        help='visualize features')
+    parser.add_argument('--update', action='store_true',
+                        help='update all models')
+    parser.add_argument('--project', default=ROOT /
+                        'runs/detect', help='save results to project/name')
+    parser.add_argument('--name', default='exp',
+                        help='save results to project/name')
+    parser.add_argument('--exist-ok', action='store_true',
+                        help='existing project/name ok, do not increment')
+    parser.add_argument('--line-thickness', default=3,
+                        type=int, help='bounding box thickness (pixels)')
+    parser.add_argument('--hide-labels', default=False,
+                        action='store_true', help='hide labels')
+    parser.add_argument('--hide-conf', default=True,
+                        action='store_true', help='hide confidences')
+    parser.add_argument('--half', action='store_true',
+                        help='use FP16 half-precision inference')
+    parser.add_argument('--dnn', action='store_true',
+                        help='use OpenCV DNN for ONNX inference')
+    parser.add_argument('--vid-stride', type=int, default=1,
+                        help='video frame-rate stride')
+    opt = parser.parse_args()
+    opt.imgsz *= 2 if len(opt.imgsz) == 1 else 1  # expand
+    source = str(opt.source)
+    save_img = not opt.nosave and not source.endswith(
+        '.txt')  # save inference images
+    is_file = Path(source).suffix[1:] in (IMG_FORMATS + VID_FORMATS)
+    is_url = source.lower().startswith(('rtsp://', 'rtmp://', 'http://', 'https://'))
+    webcam = source.isnumeric() or source.endswith(
+        '.txt') or (is_url and not is_file)
+    screenshot = source.lower().startswith('screen')
+    if is_url and is_file:
+        source = check_file(source)  # download
+    # Directories
+    save_dir = increment_path(
+        Path(opt.project) / opt.name, exist_ok=opt.exist_ok)  # increment run
+    (save_dir / 'labels' if opt.save_txt else save_dir).mkdir(parents=True,
+                                                              exist_ok=True)  # make dir
+    # Load model
+    device = select_device(opt.device)
+    model = DetectMultiBackend(
+        opt.weights, device=device, dnn=opt.dnn, data=opt.data, fp16=opt.half)
+    stride, names, pt = model.stride, model.names, model.pt
+    imgsz = check_img_size(opt.imgsz, s=stride)  # check image size
+    # Dataloader
+    # bs = 1  # batch_size
+    cnt = 0
+    if webcam:
+        view_img = check_imshow(warn=True)
+        dataset = LoadStreams(source, img_size=imgsz,
+                              stride=stride, auto=pt, vid_stride=opt.vid_stride)
+        bs = len(dataset)
+    elif screenshot:
+        dataset = LoadScreenshots(
+            source, img_size=imgsz, stride=stride, auto=pt)
+    else:
+        dataset = LoadImages(source, img_size=imgsz,
+                             stride=stride, auto=pt, vid_stride=opt.vid_stride)
+    vid_path, vid_writer = [None] * bs, [None] * bs
+    # Run inference
+    model.warmup(imgsz=(1 if pt or model.triton else bs, 3, *imgsz))  # warmup
+    seen, windows, dt = 0, [], (Profile(), Profile(), Profile())
+    for path, im, im0s, vid_cap, s in dataset:
+        with dt[0]:
+            im = torch.from_numpy(im).to(model.device)
+            im = im.half() if model.fp16 else im.float()  # uint8 to fp16/32
+            im /= 255  # 0 - 255 to 0.0 - 1.0
+            if len(im.shape) == 3:
+                im = im[None]  # expand for batch dim
+        # Inference
+        with dt[1]:
+            visualize = increment_path(
+                save_dir / Path(path).stem, mkdir=True) if opt.visualize else False
+            pred = model(im, augment=opt.augment, visualize=opt.visualize)
+        # NMS
+        with dt[2]:
+            pred = non_max_suppression(
+                pred, opt.conf_thres, opt.iou_thres, opt.classes, opt.agnostic_nms, max_det=opt.max_det)
+        # Second-stage classifier (optional)
+        # pred = utils.general.apply_classifier(pred, classifier_model, im, im0s)
+        # Process predictions
+        for i, det in enumerate(pred):  # per image
+            seen += 1
+            if webcam:  # batch_size >= 1
+                p, im0, frame = path[i], im0s[i].copy(), dataset.count
+                s += f'{i}: '
+            else:
+                p, im0, frame = path, im0s.copy(), getattr(dataset, 'frame', 0)
+            p = Path(p)  # to Path
+            save_path = str(save_dir / p.name)  # im.jpg
+            txt_path = str(save_dir / 'labels' / p.stem) + \
+                ('' if dataset.mode == 'image' else f'_{frame}')  # im.txt
+            s += '%gx%g ' % im.shape[2:]  # print string
+            # normalization gain whwh
+            gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]
+            imc = im0.copy() if opt.save_crop else im0  # for save_crop
+            annotator = Annotator(
+                im0, line_width=opt.line_thickness, example=str(names))
+            if len(det):
+                # Rescale boxes from img_size to im0 size
+                det[:, :4] = scale_boxes(
+                    im.shape[2:], det[:, :4], im0.shape).round()
+                # Print results
+                for c in det[:, 5].unique():
+                    n = (det[:, 5] == c).sum()  # detections per class
+                    # add to string
+                    s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "
+                # Write results
+                for *xyxy, conf, cls in reversed(det):
+                    if opt.save_txt:  # Write to file
+                        xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)
+                                          ) / gn).view(-1).tolist()  # normalized xywh
+                        # label format
+                        line = (
+                            cls, *xywh, conf) if opt.save_conf else (cls, *xywh)
+                        with open(f'{txt_path}.txt', 'a') as f:
+                            f.write(('%g ' * len(line)).rstrip() % line + '\n')
+                    if save_img or opt.save_crop or view_img:  # Add bbox to image
+                        c = int(cls)  # integer class
+                        label = None if opt.hide_labels else (
+                            names[c] if opt.hide_conf else f'{names[c]} {conf:.2f}')
+                        annotator.box_label(xyxy, label, color=colors(c, True))
+                    if opt.save_crop:
+                        save_one_box(xyxy, imc, file=save_dir / 'crops' /
+                                     names[c] / f'{p.stem}.jpg', BGR=True)
+            # Stream results
+            im0 = annotator.result()
+            if view_img:
+                if platform.system() == 'Linux' and p not in windows:
+                    windows.append(p)
+                    # allow window resize (Linux)
+                    cv2.namedWindow(str(p), cv2.WINDOW_NORMAL |
+                                    cv2.WINDOW_KEEPRATIO)
+                    cv2.resizeWindow(str(p), im0.shape[1], im0.shape[0])
+                cv2.putText(im0, 'Trolley:'+str(len(det)), (50, 50),
+                            cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 2, cv2.LINE_AA)
+                ret, buffer = cv2.imencode('.jpg', im0)
+                response = json.dumps({
+                    "frame": base64.b64encode(buffer).decode("utf-8"),
+                    "det": len(det),
+                    "timing": f"{dt[1].dt * 1E3:.1f}ms"
+                })
+                ws.send(response)
+        LOGGER.info(
+            f"{s}{'' if len(det) else '(no detections), '}{dt[1].dt * 1E3:.1f}ms")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
